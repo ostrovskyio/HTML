3d1d012f16513c1d0a4fe14612b3f60e49f03203
ostrovskyio <ostrovskyoleg2001@gmail.com>
2023-04-25 22:16:41 +0300

    Add:_classes

--- skript.js
+++ skript.js
@@ -1,24 +1,51 @@
-function loadProjects() {
-  const request = new XMLHttpRequest();
-  request.open(
-    "GET",
-    "https://api.github.com/users/ostrovskyio/repos",
-    true
-  );
-  request.onload = function () {
-    const repos = JSON.parse(this.response);
-    const projectsList = document.getElementById("projects");
-    repos.forEach((repo) => {
-      const project = document.createElement("li");
-      const link = document.createElement("a");
-      link.href = repo.html_url;
-      link.textContent = repo.name;
-      project.appendChild(link);
-      projectsList.appendChild(project);
-    });
-  };
-  request.send();
+class GitHubApi {
+	constructor(token, username) {
+		this.token = token;
+		this.username = username;
+	}
+
+	async getRepos() {
+		const response = await fetch(`https://api.github.com/users/${this.username}/repos`, {
+			headers: {
+				Authorization: `Basic ${btoa(`${this.username}:${this.token}`)}`,
+			},
+		});
+		const data = await response.json();
+		return data;
+	}
 }
+window.addEventListener("load", async () => {
+	const token = "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";
+	const username = "ostrovskyio";
+
+	const api = new GitHubApi(token, username);
+	const repos = await api.getRepos();
+
+	const reposList = document.getElementById("repos-list");
+	repos.forEach((repo) => {
+		const repoLink = document.createElement("a");
+		repoLink.href = repo.html_url;
+		repoLink.textContent = repo.name;
+
+		const repoListItem = document.createElement("li");
+		repoListItem.appendChild(repoLink);
+
+		reposList.appendChild(repoListItem);
+	});
+});
+
+api.getRepos().then(repos => {
+  repos.forEach(repo => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = repo.html_url;
+    a.textContent = repo.name;
+    li.appendChild(a);
+    repoList.appendChild(li);
+  });
+});
+
+
 
 function zoomIn(img) {
     if (img.style.width === '150%') {
