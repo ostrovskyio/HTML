8b6e0fa005a386a52ddce7d1fb7b79f7d610cb38
ostrovskyio <ostrovskyoleg2001@gmail.com>
2023-04-25 20:55:32 +0300

    Add_projects

--- skript.js
+++ skript.js
@@ -1,3 +1,25 @@
+function loadProjects() {
+  const request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    "https://api.github.com/users/ostrovskyio/repos",
+    true
+  );
+  request.onload = function () {
+    const repos = JSON.parse(this.response);
+    const projectsList = document.getElementById("projects");
+    repos.forEach((repo) => {
+      const project = document.createElement("li");
+      const link = document.createElement("a");
+      link.href = repo.html_url;
+      link.textContent = repo.name;
+      project.appendChild(link);
+      projectsList.appendChild(project);
+    });
+  };
+  request.send();
+}
+
 function zoomIn(img) {
     if (img.style.width === '150%') {
       zoomOut(img);
